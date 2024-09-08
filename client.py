d715b787cc5d203b75e4b7c26a1e0a7a66a09f57
bhimrazy <bhimrajyadav977@gmail.com>
2024-09-08 23:19:23 +0545

    Refactor import statement in client.py

--- client.py
+++ client.py
@@ -8,7 +8,7 @@ import numpy as np
 import pyaudio
 import requests
 import librosa
-from utils.vad import get_speech_timestamps, collect_chunks, VadOptions
+from mini_omni.utils.vad import get_speech_timestamps, collect_chunks, VadOptions
 
 API_URL = "https://8000-01j78y8pf1nes8jqpy1w44qsgw.cloudspaces.litng.ai/chat"
 
@@ -25,6 +25,7 @@ OUT_RATE = 24000
 OUT_CHUNK = 5760
 
 CURSOR_CHARS = ["|", "/", "-", "\\"]
+
 
 def run_vad(ori_audio, sr):
     _st = time.time()
@@ -54,9 +55,12 @@ def run_vad(ori_audio, sr):
         print(msg)
         return -1, ori_audio, round(time.time() - _st, 4)
 
+
 def play_audio(response):
     audio = pyaudio.PyAudio()
-    stream = audio.open(format=OUT_FORMAT, channels=OUT_CHANNELS, rate=OUT_RATE, output=True)
+    stream = audio.open(
+        format=OUT_FORMAT, channels=OUT_CHANNELS, rate=OUT_RATE, output=True
+    )
     try:
         for chunk in response.iter_content(chunk_size=OUT_CHUNK):
             if chunk:
@@ -69,6 +73,7 @@ def play_audio(response):
         stream.close()
         audio.terminate()
 
+
 def send_audio(audio_data):
     encoded_audio = base64.b64encode(audio_data).decode("utf-8")
     response = requests.post(API_URL, json={"audio": encoded_audio}, stream=True)
@@ -77,9 +82,16 @@ def send_audio(audio_data):
     else:
         print(f"Request failed with status code {response.status_code}")
 
+
 def record_and_send():
     audio = pyaudio.PyAudio()
-    stream = audio.open(format=IN_FORMAT, channels=IN_CHANNELS, rate=IN_RATE, input=True, frames_per_buffer=IN_CHUNK)
+    stream = audio.open(
+        format=IN_FORMAT,
+        channels=IN_CHANNELS,
+        rate=IN_RATE,
+        input=True,
+        frames_per_buffer=IN_CHUNK,
+    )
     print("Recording... Press Ctrl+C to stop.")
     temp_audio = b""
     start_talking = False
@@ -92,7 +104,9 @@ def record_and_send():
 
             if len(temp_audio) > IN_SAMPLE_WIDTH * IN_RATE * IN_CHANNELS * VAD_STRIDE:
                 dur_vad, vad_audio_bytes, time_vad = run_vad(temp_audio, IN_RATE)
-                print(f"duration_after_vad: {dur_vad:.3f} s, time_vad: {time_vad:.3f} s")
+                print(
+                    f"duration_after_vad: {dur_vad:.3f} s, time_vad: {time_vad:.3f} s"
+                )
 
                 if dur_vad > 0.2 and not start_talking:
                     if last_temp_audio is not None:
@@ -113,12 +127,14 @@ def record_and_send():
         stream.close()
         audio.terminate()
 
+
 def rotating_cursor():
     while True:
         for char in CURSOR_CHARS:
             sys.stdout.write(f"\rListening {char}")
             sys.stdout.flush()
             time.sleep(0.1)
+
 
 if __name__ == "__main__":
     cursor_thread = threading.Thread(target=rotating_cursor)
